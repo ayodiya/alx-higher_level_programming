3acd083f69f9c667a3cee68825801127aefda35e
ayodiya <devayodiya@gmail.com>
2021-09-09 12:01:19 +0000

    fix task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-myList = dir(hidden_4)
-
 if __name__ == '__main__':
-    for name in sorted(myList):
-        if name[0] != '_':
-            print(name)
+    import hidden_4
+    for item in dir(hidden_4):
+        if item[0:2] != "__":
+            print("{:s}".format(item))
