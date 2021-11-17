419211510db4e8b4902f603848aea9ab5be61cee
ayodiya <devayodiya@gmail.com>
2021-11-17 12:39:27 -0800

    add task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- CREATE table 'force_name'
+-- id INT, name VARCHAR(256) can't be null
+-- if table alrady exists , script should not fail
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
