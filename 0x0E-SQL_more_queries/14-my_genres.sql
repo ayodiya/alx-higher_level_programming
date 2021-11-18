d024abb370a804d3adbd18bfe11460dbb3347081
ayodiya <devayodiya@gmail.com>
2021-11-18 09:22:42 -0800

    add task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres m ON tv_genres.id = m.genre_id
+INNER JOIN tv_shows s ON m.show_id = s.id
+WHERE s.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
