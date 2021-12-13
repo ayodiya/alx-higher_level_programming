fbf94c1802bf353e22642cc360bd6ed19a537269
ayodiya <devayodiya@gmail.com>
2021-12-13 17:00:00 +0100

    fix conditions to return 1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function factorial (num) {
-  if (num === 0 || num === 1 || isNaN(num)) { return 1; }
+  if (isNaN(num)) { return 1; }
   for (let i = num - 1; i >= 1; i--) {
     num *= i;
   }
