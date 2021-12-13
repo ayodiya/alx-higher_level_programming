76a424754198788366e60f5b0d10259377e7f3d9
ayodiya <devayodiya@gmail.com>
2021-12-13 16:35:14 +0100

    add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
+const num = process.argv[2];
+for (let i = 0; i < num; i++) {
+  console.log('x'.repeat(num));
+}
