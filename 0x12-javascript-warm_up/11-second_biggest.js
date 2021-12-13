7df2356d5dacc7f992c967de11e4992acd41cd75
ayodiya <devayodiya@gmail.com>
2021-12-13 18:06:41 +0100

    add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const argsLength = process.argv.length - 2;
+const argArray = [];
+
+for (let i = 0; i < argsLength; i++) {
+  argArray.push(process.argv[2 + i]);
+}
+
+argArray.sort();
+
+if (argsLength === 0 || argsLength === 1) {
+  console.log(0);
+} else {
+  console.log(argArray[argArray.length - 2]);
+}
