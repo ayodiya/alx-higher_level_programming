8cde5b2879afe76922041638ac0c2ac651651b0c
ayodiya <devayodiya@gmail.com>
2021-12-13 06:03:40 -0800

    lint with semi standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const argsNum = process.argv.length - 2
+const argsNum = process.argv.length - 2;
 
-if (!argsNum){
-    console.log('No argument')
-}
-else if(argsNum === 1){
-    console.log('Argument found')
-}else{
-    console.log('Arugments found')
+if (!argsNum) {
+  console.log('No argument');
+} else if (argsNum === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arugments found');
 }
