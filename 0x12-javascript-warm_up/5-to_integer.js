296b0dbf18a190446c1af1d2fdf51cabb239dd91
ayodiya <devayodiya@gmail.com>
2021-12-13 16:08:49 +0100

    remove comment

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
-// console.log(parseInt('school'));
-// console.log(`${process.argv[2]} is ${process.argv[3]}`);
-
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
