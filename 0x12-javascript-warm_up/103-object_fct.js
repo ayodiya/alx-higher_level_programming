c5694978144297104b046aa694e21f30bb4f508d
ayodiya <devayodiya@gmail.com>
2021-12-13 18:44:43 +0100

    add task 17

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: () => myObject.value++
+};
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
